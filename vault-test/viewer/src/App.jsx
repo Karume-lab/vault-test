c11c120a79d5412487f63d8f897213acfe9d5de1
EphraimShikanga <ephraimshikanga@gmail.com>
2024-03-29 18:38:42 +0300

    file viewing

--- vault-test/viewer/src/App.jsx
+++ vault-test/viewer/src/App.jsx
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import BasicTable from './components/BasicTable';
+import logo from "./logo.svg";
+import "./App.css";
+import BasicTable from "./components/BasicTable";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
 function App() {
+  const docs = [{ uri: require("./files/w.jpg"), fileType: "jpg" }];
   return (
     <div className="App">
       <BasicTable />
+      <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />
     </div>
   );
 }
